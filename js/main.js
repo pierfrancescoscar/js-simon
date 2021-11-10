a3fc168822eb522531c3d572b6a7a720cbe2991b
pierfrancescoscar <scarinci401@gmail.com>
2021-11-10 14:06:54 +0100

    ADD: general exercise setup

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+// Dopo 30 secondi i numeri in pagina devono essere rimossi e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// Refs
+
+// Functions - Gen random number from 1 to 100
+
+function genRandNumber (min, max) {
+    return Math.floor(Math.random() * max) + min;
+}
